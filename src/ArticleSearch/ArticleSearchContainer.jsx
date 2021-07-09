a44911a883bb4e8ea33e32325c27f072e689d8e3
Lukasz Korzeniowski <lkorzeniowski2@objectivity.co.uk>
2021-07-09 13:41:05 +0200

    Exchange mocking data for fetching it from wikipedia api.

--- src/ArticleSearch/ArticleSearchContainer.jsx
+++ src/ArticleSearch/ArticleSearchContainer.jsx
@@ -1,36 +1,78 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticleSearchBar from './ArticleSearchBar';
 import Article from './Article';
-import { articles } from './resources/Articles';
-import { Grid } from '@material-ui/core';
+// import { articles } from './resources/Articles';
+import { Grid, CircularProgress } from '@material-ui/core';
+
 
 
 const ArticleSearchContainer = () => {
     const [searchValue, setSearchValue] = useState('')
-
-    const [filteredArticles, setFilteredArticles] = useState([])
+    const [status, setStatus] = useState('idle')
+    const isLoading = status === 'loading'
+    const isSuccess = status === 'success'
+    const isError = status === 'error'
+    const [articlesData, setArticlesData] = useState(null)
+    const [queried, setQueried] = useState(false)
+    const [error, setError] = useState(false)
     
     const handleChange = (e) => {
         const {value} = e.target
-        debugger
         setSearchValue(value.toLowerCase())
+        setQueried(true)
     }
 
-    const handleSearch = useCallback(() => {
-        const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(searchValue))
-        setFilteredArticles(filteredArticles)
-    },[searchValue])
-
     useEffect(() => {
-        handleSearch()
-    }, [searchValue, handleSearch])
+        if (!queried) {
+            return
+        }
+        console.log('serach value ', searchValue)
+        setStatus('loading')
+        // const fetchData = () => {
+            window.fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${encodeURIComponent(searchValue)}`)
+                .then(async response => {
+                    const data = await response.json()
+                    if (response.ok) {
+                        return data
+                    } else {
+                        return Promise.reject(data)
+                    }
+                })
+                .then(
+                    responseData => {
+                        setArticlesData(responseData)
+                        setStatus('success')
+                    },
+                    errorData => {
+                        setError(errorData)
+                        setStatus('error')
+                    }
+                )
+    
+                
+            // }
+        // fetchData()
+
+        // fetchData jest async
+        // a w środku windoew fetch jest na await
+
+
+    },[searchValue, queried])
 
     return (
-        <Grid container spacing={2} item xs={6} md={6}>
+        <Grid container spacing={2}>
             <ArticleSearchBar searchValue={searchValue} handleChange={handleChange} />
-            {filteredArticles.map((article) => (
-                <Article key={article.pageid} title={article.title} snippet={article.snippet} url={article.url} />
-            ))}
+            {isError ? error.message : null}
+            {isLoading ? <CircularProgress /> : null}
+            {isSuccess ? (
+                articlesData?.query?.search?.length ? (
+                    articlesData.query.search.map((article) => (
+                        <Article key={article.pageid} article={article} />
+                    )) 
+                ) : (
+                    <p>No records found</p>
+                )
+            ) : null}
         </Grid>
     )
 };
